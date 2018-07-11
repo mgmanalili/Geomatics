f9b4f99afa4340a93f6ae6c728c16150454f6de6
Michael Andrew Manalili <michaelandrew.manalili@gmail.com>
2018-07-11 15:57:55 +0200

    Update geoenabler_final_v1.py

--- geoenabler_final_v1.py
+++ geoenabler_final_v1.py
@@ -1,15 +1,9 @@
+#This is was used to assign use case specific id to tableA based on existing tableB.
+#Table A should take the last item in tableB-ID and continue the counting based on a specific column name.
 
 # coding: utf-8
-
-# In[12]:
-
-
 from pandas import DataFrame, read_csv
 import pandas as pd
-
-
-# In[13]:
-
 
 s = r'/Users/michael/Desktop/Geoenabler_SCOPE/test.txt'
 d = r'/Users/michael/Desktop/Geoenabler_SCOPE/raw_data.txt'
@@ -17,27 +11,15 @@ df_s = pd.read_csv(s, sep = "\t")
 df_d = pd.read_csv(d, sep = "\t")
 print df_d
 
-
-# In[17]:
-
-
 df_d.reset_index(drop=True, inplace=True)
 df_s.reset_index(drop=True, inplace=True)
 df_d['cum_count'] = df_d.groupby('iso3d').cumcount() + 1
+
 #Drop duplicates keeps the last record
 dupli = df_s.drop_duplicates(['iso3_s'],keep='last')
 print dupli
 
-
-# In[15]:
-
-
 df_d['locid_s'] = df_d['iso3d'].map(dupli.set_index('iso3_s')['loc_id_s'])
 print df_d
 
-
-# In[16]:
-
-
 df_d.to_csv(r'/Users/michael/Desktop/result.txt')
-
